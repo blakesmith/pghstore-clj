2134dcd77c0ac777b92e955a33ad70ce8a01e64b
Blake Smith <blakesmith0@gmail.com>
2012-06-27 22:25:01 -0500

    PGobject to hash first pass

--- src/pghstore_clj/core.clj
+++ src/pghstore_clj/core.clj
@@ -1,12 +1,16 @@
 (ns pghstore-clj.core
+  (:require [clojure.string :as st])
   (:import [org.postgresql.util PGobject]))
 
-(defprotocol Hstorable
-  (hstore [this]))
+(defprotocol THstorable
+  (to-hstore [this]))
+
+(defprotocol FHstorable
+  (from-hstore [this]))
 
 (extend-type clojure.lang.PersistentArrayMap
-  Hstorable
-  (hstore [this]
+  THstorable
+  (to-hstore [this]
     (doto (PGobject.)
       (.setType "hstore")
       (.setValue
@@ -15,4 +19,12 @@
                          (for [[k v] this]
                            (format "\"%s\"=>\"%s\"" (name k) (name v)))))))))
 
-                         
+(extend-type org.postgresql.util.PGobject
+  FHstorable
+  (from-hstore [this]
+    (into {}
+          (for [[k v]
+                (map (fn [v]
+                       (map #(st/replace % #"\"" "") (st/split v #"=>")))
+                     (st/split (.getValue this) #", "))]
+            [(keyword k) v]))))
--- test/pghstore_clj/core_test.clj
+++ test/pghstore_clj/core_test.clj
@@ -4,7 +4,13 @@
 
 (deftest test-hash-to-hstore
   (testing "creating a PGobject with the correct value"
-    (is (= "hstore" (.getType (hstore {:color "blue"})))))
+    (is (= "hstore" (.getType (to-hstore {:color "blue"})))))
   (testing "storing the correct value in the PGobject"
     (is (= "\"name\"=>\"test\", \"username\"=>\"root\""
-           (.getValue (hstore {:name "test" :username "root"}))))))
+           (.getValue (to-hstore {:name "test" :username "root"}))))))
+
+(deftest test-hstore-to-hash
+  (let [pgo (to-hstore {:name "test" :username "root"})]
+    (testing "creating a hash from a PGobject"
+      (is (= {:name "test" :username "root"} (from-hstore pgo))))))
+    
