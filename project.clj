04b095cc957d4c7b486bdf7473eb9c588c287a5c
Blake Smith <blakesmith0@gmail.com>
2012-06-30 18:05:21 -0500

    Add project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pghstore-clj "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Helper functions for working with PostgresSQL hstore data type in Clojure via JDBC"
+  :url "http://github.com/blakesmith/pghstore-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
